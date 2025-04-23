8931725c06bdb787fd849ed0431fba7eebd29912
eminalparslan <eminalparslan@gmail.com>
2025-04-23 18:26:21 -0400

    fix settings ui

--- app/auth/[pathname]/page.tsx
+++ app/auth/[pathname]/page.tsx
@@ -8,7 +8,7 @@ export function generateStaticParams() {
 export default async function AuthPage({ params }: { params: Promise<{ pathname: string }> }) {
     const { pathname } = await params
 
-    return (<div className="self-center">
+    return (<div className="self-center min-w-3/4">
         <AuthView pathname={pathname} />
     </div>)
 }
